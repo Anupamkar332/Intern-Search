49d4aa01cbd4f33744d24622d8bd176778727287
Anupamkar332 <72396252+Anupamkar332@users.noreply.github.com>
2020-10-19 10:42:35 +0530

    Add files via upload

---
+++ factorial_by_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+long int factorial(int n);
+int main()
+{
+	int n;
+	printf("Enter your number:");
+	scanf("%d", &n);
+	printf("factorial of the given number %d = %Id", n, factorial(n));
+	return 0;
+}
+long int factorial(int n)
+{
+    if (n>=1)
+        return n*factorial(n-1);
+    else
+        return 1;
+}
+
